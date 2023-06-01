d6c69748de0e7ecb7fdd1adb2701963a13ac9128
Xavier Maximin <xavier.maximin.uk@gmail.com>
2023-06-01 02:38:54 -0400

    feat(ui): added team name under the image of the team

--- app/src/main/java/com/kodesparkle/sportdbfdj/presentation/screens/team_list/components/TeamComponent.kt
+++ app/src/main/java/com/kodesparkle/sportdbfdj/presentation/screens/team_list/components/TeamComponent.kt
@@ -2,16 +2,20 @@ package com.kodesparkle.sportdbfdj.presentation.screens.team_list.components
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
@@ -33,10 +37,21 @@ fun TeamComponent(
         ConstraintLayout(
             modifier = Modifier.fillMaxWidth()
         ) {
+
+            val (teamImage, teamName) = createRefs()
+
             Box(
-                modifier = Modifier.fillMaxWidth(),
-                contentAlignment = Alignment.Center
-            ) {
+                modifier = Modifier
+                    .padding(top = 8.dp)
+                    .constrainAs(teamImage) {
+                        top.linkTo(parent.top)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    }
+                    .fillMaxWidth(),
+                contentAlignment = Alignment.Center,
+
+                ) {
                 AsyncImage(
                     modifier = Modifier.fillMaxWidth(),
                     model = ImageRequest.Builder(LocalContext.current)
@@ -46,7 +61,23 @@ fun TeamComponent(
                     contentScale = ContentScale.Crop,
                     contentDescription = team.name
                 )
+
             }
+            Text(
+                text = team.name,
+                modifier = Modifier
+                    .padding(top = 12.dp)
+                    .constrainAs(teamName) {
+                        top.linkTo(teamImage.bottom)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    }
+                    .fillMaxWidth()
+                    .wrapContentHeight(),
+                color = Color.Black,
+                style = MaterialTheme.typography.labelSmall,
+                textAlign = TextAlign.Center
+            )
         }
 
     }
